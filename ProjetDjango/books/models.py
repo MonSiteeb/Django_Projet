ce032e36ee97bd1907c3123d7aab7fee612c6f9a
amine-13 <aminegbadamassi@gmail.com>
2022-02-24 23:58:08 +0000

    Creation du model Documents

--- ProjetDjango/books/models.py
+++ ProjetDjango/books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Documents(models.Model):
+    catchoice= [
+        ('epreuve', 'Epreuve'),
+        ('correction', 'Correction'),
+        ]
+    matiere=models.CharField(max_length=30)
+    classe=models.CharField(max_length=30)
+    professeur=models.CharField(max_length=40)
+    categorie=models.CharField(max_length=30,choices=catchoice,default='epreuve')
+    fichier=models.FileField()
+    def __str__(self):
+        return str(self.name)+"["+str(self.classe)+']'
