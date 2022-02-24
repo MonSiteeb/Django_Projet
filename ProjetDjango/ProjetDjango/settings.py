5a7cf386696b319deb57a99b3589a5fbbec8d459
amine-13 <aminegbadamassi@gmail.com>
2022-02-24 23:56:03 +0000

    Administrateur

--- ProjetDjango/ProjetDjango/settings.py
+++ ProjetDjango/ProjetDjango/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+
 
 from pathlib import Path
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
+
 ]
 
 MIDDLEWARE = [
@@ -117,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
