d0dd4c8c2ec54d40a36f66614e9e8b50a6bbe96b
diatou beye <khadidiatoubeye@hotmail.com>
2022-02-25 15:53:43 +0000

    modif

--- ProjetDjango/ProjetDjango/urls.py
+++ ProjetDjango/ProjetDjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from books.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',login),
+    path('inscription',inscription),
+    path('ajoutEpreuve',ajoutEpreuve),
+    
 ]
--- ProjetDjango/books/views.py
+++ ProjetDjango/books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request,'login.html')
+
+def inscription(request):
+    return render(request,'inscription.html') 
+
+def ajoutEpreuve(request):
+    return render(request,'ajout_epreuve.html') 
