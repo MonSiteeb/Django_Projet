f68b292a7d93559c37a844a8146e147cf840c2d7
amine-13 <aminegbadamassi@gmail.com>
2022-02-25 10:46:48 +0000

    test

--- ProjetDjango/books/admin.py
+++ ProjetDjango/books/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Documents
+from .models import Documents,Newsletter,MailMessage
 admin.site.register(Documents)
+admin.site.register(MailMessage)
+admin.site.register(Newsletter)
