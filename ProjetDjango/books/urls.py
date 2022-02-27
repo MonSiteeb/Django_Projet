779e77cf90272dca14221241df5d7159c7da9106
amine-13 <aminegbadamassi@gmail.com>
2022-02-27 15:45:25 +0000

    m

--- ProjetDjango/books/urls.py
+++ ProjetDjango/books/urls.py
@@ -1,9 +1,16 @@
 from books.views import index, subscribe
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     # path('', views.index, name='index'),
     path("",index,name="index"),
     path('contact',subscribe,
          name='contact')
+]
+
+#Add Django site authentication urls (for login, logout, password management)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
