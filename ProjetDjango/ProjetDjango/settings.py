1b21061f46898c3add116362b75435d2d64e06c1
amine-13 <aminegbadamassi@gmail.com>
2022-03-05 22:55:27 +0000

    m

--- ProjetDjango/ProjetDjango/settings.py
+++ ProjetDjango/ProjetDjango/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL='/afterlogin'
+LOGOUT_REDIRECT_URL = '/login'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL = '/media/'
 
--- ProjetDjango/ProjetDjango/urls.py
+++ ProjetDjango/ProjetDjango/urls.py
@@ -18,9 +18,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from books import views
+from django.conf.urls import include
+
+
+from django.contrib.auth.views import LoginView,LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('books.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('afterlogin', views.afterlogin),
+    path('login', LoginView.as_view(template_name='registration/login.html')),
+    path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
+
+
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
